6236179bc6fcafebf66c7191756e4914a79a76c3
Daniel Alamilla <daniel.alamilla.co@gmail.com>
2021-04-01 18:19:22 -0600

    add second problem of euler project with unittest

--- euler/euler1-10_test.go
+++ euler/euler1-10_test.go
@@ -17,8 +17,29 @@ func TestEuler001(t *testing.T){
 	for _, eu := range Euler001Tests {
 		got := Euler001(eu.input)
 		if got != eu.result {
-				t.Errorf("Euler001(%d) => %d, should return %d", eu.input, eu.result, got)
+				t.Errorf("Euler001(%d) => %d, should return %d", eu.input, got, eu.result)
 		}
 	}
+}
 
+func TestEuler002(t *testing.T){
+	var Euler002Tests = []struct {
+		input  int
+		result int
+	}{
+		{8, 10},
+		{10, 10},
+		{34, 44},
+		{60, 44},
+		{1000, 798},
+		{100000, 60696},
+		{4000000, 4613732},
+	}
+
+	for _, eu := range Euler002Tests {
+		got := Euler002(eu.input)
+		if got != eu.result {
+			t.Errorf("Euler002(%d) => %d, should return %d", eu.input, got, eu.result)
+		}
+	}
 }
